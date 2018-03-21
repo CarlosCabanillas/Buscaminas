65f2355285ce7afe653310a76f6bb487d8125673
Carlos Cabanillas <32900038+CarlosCabanillas@users.noreply.github.com>
2018-03-21 13:40:38 +0100

    Add files via upload

--- buscaminas.cpp
+++ buscaminas.cpp
@@ -6,28 +6,33 @@
 #define Q 10
 
 void pon_minas(int a[M][N]){
-    
-	for(int minas_puestas=0; minas_puestas<Q; minas_puestas++){
-	do{	
-	    int f = rand() % M;
-	    int c = rand() % N;
-	} while(a[f][c]!=0);
-	 
-	a[f][c] = 1;
-	
-	}
+
+      for(int minas_puestas=0; minas_puestas<Q; minas_puestas++){
+          int fila;
+          int colum;
+          do{
+                 fila = rand() % M;
+                 colum = rand() % N;
+            } while(a[fila][colum]!=0);
+
+          a[fila][colum] = 1;
+
+      }
 }
 void rellena(int a[M][N]){
-    for (int f=0; f<M; f++)
-	for(int c=0; c<M; c++)
-	    a[f][c] = 0;
+    for (int fila=0; fila<M; fila++)
+        for(int colum=0; colum<M; colum++)
+            a[fila][colum] = 0;
 }
 
 int main(){
     int tablero[M][N];
-
     rellena(tablero);
     pon_minas(tablero);
-
     return EXIT_SUCCESS;
 }
+
+
+
+
+
