72882fae1644e02b6f5bd249d2900802ddde6591
Carlos Cabanillas <32900038+CarlosCabanillas@users.noreply.github.com>
2018-04-06 13:05:57 +0200

    Add files via upload
    Añadido la matriz de las X para que las imprima en pantalla, falta meter para que una vez introducida la coordenada aparezca lo que hay debajo

--- buscaminas.cpp
+++ buscaminas.cpp
@@ -1,17 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define M 6
 #define N 6
 #define Q 10
 
-void pon_minas(int a[M][N]){
+void pon_minas(int a[N][N]){
 
       for(int minas_puestas=0; minas_puestas<Q; minas_puestas++){
           int fila;
           int colum;
           do{
-                 fila = rand() % M;
+                 fila = rand() % N;
                  colum = rand() % N;
             } while(a[fila][colum]!=0);
 
@@ -19,15 +18,15 @@ void pon_minas(int a[M][N]){
 
       }
 }
-void rellena(int a[M][N]){
-    for (int fila=0; fila<M; fila++)
-        for(int colum=0; colum<M; colum++)
+void rellena(int a[N][N]){
+    for (int fila=0; fila<N; fila++)
+        for(int colum=0; colum<N; colum++)
             a[fila][colum] = 0;
 }
 
-void toti(int tablero[M][N]){
+void toti(int tablero[N][N]){
 	int x,y;
-	for (x=0; x<M; x++){
+	for (x=0; x<N; x++){
 		for (y=0; y<N; y++){
 			printf("%d", tablero[x][y]);
 }
@@ -35,13 +34,21 @@ void toti(int tablero[M][N]){
 }		
 }
 
-
+void matriz_x(int a[N][N]){
+	char letra = 'x';
+	for(int f=0; f<N;f++) {
+		for(int c=0; c<N; c++)
+			printf("%c", letra);
+		printf("\n");
+	}
+	
+}
 
 int main(){
-    int tablero[M][N];
+    int tablero[N][N];
 	rellena(tablero);
+	matriz_x(tablero);
     pon_minas(tablero);
-	toti(tablero);
     return EXIT_SUCCESS;
 }
 
