4d7d44366e872195c4ef4f341827b399a0e330ab
Carlos Cabanillas <32900038+CarlosCabanillas@users.noreply.github.com>
2018-04-20 12:37:41 +0200

    Buscaminas Xtreme

--- buscaminas.cpp
+++ buscaminas.cpp
@@ -7,57 +7,77 @@
 void pon_minas(int a[N][N]){
 
     for(int minas_puestas=0; minas_puestas<Q; minas_puestas++){
-	int fila;
-	int colum;
-	do{
-	    fila = rand() % N;
-	    colum = rand() % N;
-	} while(a[fila][colum]!=0);
+        int fila;
+        int colum;
+        do{
+            fila = rand() % N;
+            colum = rand() % N;
+        } while(a[fila][colum]!=0);
 
-	a[fila][colum] = 1;
+        a[fila][colum] = 1;
 
     }
 }
 void rellena(int a[N][N]){
     for (int fila=0; fila<N; fila++)
-	for(int colum=0; colum<N; colum++)
-	    a[fila][colum] = 0;
+        for(int colum=0; colum<N; colum++)
+            a[fila][colum] = 0;
 }
 
 void toti(int tablero[N][N]){
     int x,y;
     for (x=0; x<N; x++){
-	for (y=0; y<N; y++){
-	    printf("%d", tablero[x][y]);
-	}
-	printf("\n");
+        for (y=0; y<N; y++){
+            printf("%d", tablero[x][y]);
+        }
+        printf("\n");
     }
 }
 
 void matriz_x(int a[N][N], int r[N][N]){
     char letra = 'x';
     for(int f=0; f<N;f++) {
-	for(int c=0; c<N; c++)
-	    if (r[f][c] == 1)
-		printf("%i", a[f][c]);
-	    else
-		printf("%c", letra);
-	printf("\n");
+        for(int c=0; c<N; c++)
+            if (r[f][c] == 1)
+                printf("%i", a[f][c]);
+            else
+                printf("%c", letra);
+        printf("\n");
     }
 
 }
 
 void revela(int tablero[N][N]) {
+
     int x, y;
+    do {
     printf("introduce (0,0): ");
     scanf("%i %*1[,] %i", &x, &y);
+    } while ((x > N-1 || y > N-1) || (x < 0 || y < 0));
     tablero[x][y] = 1;
 }
 
-int comprueba(){
-    
+int comprueba(int tablero[N][N], int revela[N][N]){
 
+    int amount = 0;
+    for(int f=0; f<N; f++)
+        for(int c = 0; c<N; c++)
+        {
+            int vtablero = tablero[f][c];
+            int rtablero = revela[f][c];
+            if(vtablero && rtablero) {
+                return 2;
+            }
+            else if(!vtablero && !rtablero)
+            {
+                amount++;
+            }
+        }
 
+    if(amount == 0)
+        return 1;
+    else
+        return 0;
 
 }
 
@@ -74,16 +94,18 @@ int main(){
         revela(revelar);
         matriz_x(tablero, revelar);
 
-        int vcomprueba = comprueba(tablero);
+        int vcomprueba = comprueba(tablero, revelar);
         if (vcomprueba == 0)
             continue;
         if (vcomprueba == 1)
         {
             //Ha ganado
+            printf("Has ganado\n");
         }
         else
         {
             //Ha perdido
+            printf("Has perdido\n");
         }
         finish = true;
     }
